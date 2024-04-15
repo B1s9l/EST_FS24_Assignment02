372aedc4f79727c5059f9a4370620e544805da75
ggerber <gianandreadaniel.gerber@uzh.ch>
2024-04-15 17:02:17 +0200

    UniquePaths including documentation and screenshots

--- Documentation.md
+++ Documentation.md
@@ -7,7 +7,34 @@
 ## SortedArray2BST (Gianni)
 ## SumofTwoIntegers (Basil)
 ## UniquePathsGrid (Gianni)
+### 1. Code Coverage
+Since the code of uniquePaths does not contain any branches, achieving 100 % line and branch coverage can be done with just one test,
+and it does not even matter which numbers one chooses since they are constrained to be >= 1 and <= 100 therefor the loops will always execute, immediately resulting in 100% code coverage. I added 4 tests for this function tho in order to verify that the function also works correctly for bigger numbers(which might be redundant).
+### 2. Designing Contracts
+As a precondition, as specified in the readme, both m and n have to be within the range [1,100], this can easily be verified with a simple if statement. 
 
+As a postcondition, as stated by the readme, it has to return a number within the range [1,INT_MAX], since having a negative amount of paths does conceptually not make sense. Additionally, since the exercise makes us of the data-type Integer, the largest number which makes sense as a result is INT_MAX, because if it exceeds this number, the result will be wrong due to int overflow, possibly also resulting in a negative number or a positive wrong result, in my example I check this within the invariant, because in the last iteration the invariant is checked aswell and verifies that the result is <= INT_Max.
+
+As an invariant, one can check at the beginning of every iteration, whether the addition of the two numbers results in a number which is less than or equal to INT_MAX, and if it exceeds this number, one could throw an exception indicating that the result cannot be calculated with this function. Changing the data type to long will not fix the problem, because the overflow just starts later. If one does really want to be able to input two numbers which are 100, one could consider changing the type to BigInteger, which will always yield the correct result, but using the BigInteger type results in a huge Performance loss.
+
+### 3. Testing Contracts
+
+testmToSmallInput: verifies that the function throws an exception if m is < 1
+
+testnToSmallInput: verifies that the function throws an exception if n is < 1
+
+testmToBigInput: verifies that the function throws an exception if m is > 100
+
+testnToBigInput: verifies that the function throws an exception if n is > 100
+
+testOverallInputOverflows: verifies that the function throws an exception as soon as the calculated result would be > INT_MAX
+
+### 4. Property-Based Testing
+Property 1: returns correct number of unique paths if it does not overflow. Note how i dynamically calculate n based on the value of m, since there is no way to guarantee no overflow otherwise. Since i only provide a range from 1-100, i added a placeholder variable k in order to generate 1000 samples, rather than just 100.
+
+Property 2: if either m or n is not in the range [1,100], an exception is thrown. This is tested by the function preConditionNotMetThrowsException, and it will test all 4 parts of the if equally since i designed a smart property.
+
+Property 3: if the addition of the two partial results of the cells results in a number greater than INT_MAX, an exception is thrown. I had to figure out the boundaries where exceptions are thrown, then i just generate a random number for m, and then decide on the smallest possible n value for which the result overflows. Note that here jqwik only generates 83 tests, which is fine in my opinion since all bigger values for n would result in an overflow aswell
 (Please create a Documentation.md file for each task.)
 ### 1. Code Coverage
 ### 2. Designing Contracts
--- UniquePathsGrid/src/test/java/zest/uniquePathsTest.java
+++ UniquePathsGrid/src/test/java/zest/uniquePathsTest.java
@@ -1,6 +1,100 @@
 package zest;
 
-class uniquePathsTest {
+import org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 
- 
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class uniquePathsTest {
+
+    private UniquePaths instance;
+    @BeforeEach
+    public void setUp(){
+        instance = new UniquePaths();
+    }
+    @Test
+    public void testBaseCaseNoActualPathInput(){
+        Assertions.assertEquals(1, instance.uniquePaths(1,1));
+    }
+    @Test
+    public void testSmallInput(){
+        Assertions.assertEquals(3, instance.uniquePaths(2,3));
+    }
+    @Test
+    public void testalmostOverflowInput(){
+        Assertions.assertEquals(1166803110, instance.uniquePaths(18,17));
+    }
+    @Test
+    public void testmToSmallInput(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{instance.uniquePaths(0,1);});
+    }
+    @Test
+    public void testnToSmallInput(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{instance.uniquePaths(1,0);});
+    }
+    @Test
+    public void testnToBigInput(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{instance.uniquePaths(1,101);});
+    }
+    @Test
+    public void testmToBigInput(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{instance.uniquePaths(101,1);});
+    }
+    @Test
+    public void testOverallInputOverflows(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{instance.uniquePaths(18,18);});
+    }
+    @Test
+    public void testlargerInputNoOverflow(){
+        Assertions.assertEquals(28, instance.uniquePaths(3,7));
+    }
+
+    @Property
+    void preConditionNotMetThrowsException(@ForAll @IntRange(min = Integer.MIN_VALUE, max = 0) int m1,
+                                           @ForAll @IntRange(min = 101, max = Integer.MAX_VALUE) int m2,
+                                           @ForAll @IntRange(min = Integer.MIN_VALUE, max = 0) int n1,
+                                           @ForAll @IntRange(min = 101, max = Integer.MAX_VALUE) int n2,
+                                           @ForAll @IntRange(min = 0,max = 3) int which){
+        instance = new UniquePaths();
+        int m = which < 2 ? (which == 0 ? m1 : m2 ) : 3;// to ensure both validations are checked and not just for m
+        int n = which > 1 ? n2 : n1; // always checked after, therefor no need to adjust values, will be implicitly return bc of m being checked first
+        assertThatThrownBy(() ->{
+            instance.uniquePaths(m,n);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Property
+    void overflowThrowsIllegalArgumentException(@ForAll @IntRange(min = 18, max = 100) int m){
+        instance = new UniquePaths();
+        int n = m > 69 ? 8 : m > 51 ? 9 : m > 41 ? 10 : m > 34 ? 11 : m > 29 ? 12: m > 26 ?13 : m > 23 ? 14: m > 21 ? 15 : m > 20 ? 16 : m > 18 ? 17 : 18;
+        assertThatThrownBy(() ->{
+            instance.uniquePaths(m,n);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+    @Property
+    void noOverflowCalculatesCorrectResult(@ForAll @IntRange(min=1, max = 100) int m, @ForAll @IntRange(max=10)int k){
+        instance = new UniquePaths();
+        int n = m > 69 ? rand(1,7): m > 51 ? rand(1,8) : m > 41 ? rand(1,9) : m > 34 ? rand(1,10) : m > 29 ?rand(1,11): m > 26 ?rand(1,12) : m > 23 ?rand(1,13): m > 21 ? rand(1,14) : m > 20 ?rand(1,15) : m > 18 ? rand(1,16) :rand(1,17);
+        int expected = numberOfPaths(m,n);
+        assertThat(instance.uniquePaths(m,n)).isEqualTo(expected);
+    }
+    private int rand(int min, int max){
+        return Arbitraries.integers().between(min, max).sample();
+    }
+    private int numberOfPaths(int m, int n)
+    {
+        long path = 1;
+        for (int i = n; i < (m + n - 1); i++) {
+            path *= i;
+            path /= (i - n + 1);
+        }
+        return (int)path;
+    }
 }
